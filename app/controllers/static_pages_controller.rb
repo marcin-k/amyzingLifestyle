502e67ae7cdc2338c6db663f6f68000df2942778
marcin <marcin.krze@icloud.com>
2018-07-19 15:11:57 +0000

    Added information about birds, insects, and reptiles.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contactUs
+  end
+
+  def aboutUs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  get 'cart/index'
+  resources :items
+  root 'static_pages#home'
+  
+  get 'static_pages/home'
+  get '/contact' => 'static_pages#contactUs'
+  get '/about' => 'static_pages#aboutUs'
+  get '/recipes' => 'static_pages#recipes'
+  get '/login' => 'user#login' 
+  get '/logout' => 'user#logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
