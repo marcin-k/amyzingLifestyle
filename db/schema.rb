791b5eabf16e8633b524fbd4c2e8ecfa47c03692
marcin-k <marcin.krze@gmail.com>
2018-07-30 19:43:04 +0000

    update devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_120038) do
+ActiveRecord::Schema.define(version: 2018_07_30_192201) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -74,6 +74,11 @@ ActiveRecord::Schema.define(version: 2018_07_28_120038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "standard"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "address"
+    t.integer "weight"
+    t.integer "height"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
